56571317663fc842b5993ff7f52189ab1f906da8
rafael <rafaelcmd@gmail.com>
2021-01-14 16:39:12 -0300

    created method  to test creation and query of categories

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,5 +1,7 @@
 import {Request, RestBindings, get, ResponseObject} from '@loopback/rest';
 import {inject} from '@loopback/core';
+import {CategoryRepository} from '../repositories';
+import {repository} from '@loopback/repository';
 
 /**
  * OpenAPI response for ping()
@@ -32,7 +34,8 @@ const PING_RESPONSE: ResponseObject = {
  * A simple controller to bounce back http requests
  */
 export class PingController {
-  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request,
+              @repository(CategoryRepository) private categoryRepo: CategoryRepository) {}
 
   // Map to `GET /ping`
   @get('/ping', {
@@ -48,5 +51,15 @@ export class PingController {
       url: this.req.url,
       headers: Object.assign({}, this.req.headers),
     };
+  }
+
+  @get('/categories')
+  async index() {
+    await this.categoryRepo.create({
+      id: '1',
+      name: 'category test',
+      description: 'category created for testing'
+    })
+    return this.categoryRepo.find()
   }
 }
