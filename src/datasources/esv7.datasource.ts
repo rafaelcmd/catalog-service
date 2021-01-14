a8e6298309a4fa68fba1133c87535585e590ac6d
rafael <rafaelcmd@gmail.com>
2021-01-14 16:04:59 -0300

    Changed .env.example, docker-compose.yaml and esv7.datasource. Created category.repository

--- src/datasources/esv7.datasource.ts
+++ src/datasources/esv7.datasource.ts
@@ -1,16 +1,20 @@
-import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
-  name: 'esv7',
-  connector: 'es',
+  name: 'esv6',
+  connector: 'esv6',
   index: 'catalog',
-  apiVersion: '5.0',
-  defaultSize: '',
+  version: 7,
+  debug: process.env.APP_ENV === 'dev',
+  //defaultSize: '',
   configuration: {
     node: process.env.ELASTIC_SEARCH_HOST,
     requestTimeout: process.env.ELASTIC_SEARCH_REQUEST_TIMEOUT,
     pingTimeout: process.env.ELASTIC_SEARCH_PING_TIMEOUT
+  },
+  mappingProperties: {
+
   }
 };
 
@@ -24,10 +28,7 @@ export class Esv7DataSource extends juggler.DataSource
   static dataSourceName = 'esv7';
   static readonly defaultConfig = config;
 
-  constructor(
-    @inject('datasources.config.esv7', {optional: true})
-    dsConfig: object = config,
-  ) {
-    super(dsConfig);
+  constructor() {
+    super(config);
   }
 }
